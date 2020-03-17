8545ad1131b8e5ca6f239becb176444d144aa0e1
germansommer <germangsommer@gmail.com>
2020-03-17 12:47:36 -0300

    Primer Test Funciona
    El primer test funciona, toca hacer todos los otros

---
+++ Mascota.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MascotaVirtual.Tests
+{
+    class Mascota
+    {
+        public Boolean EstaDespierto { set; get; }
+        public Boolean EstaLleno { set; get; }
+
+        public Boolean PuedeComer()
+        {
+            if (this.EstaLleno == true)
+            {
+                return false;
+            }
+            
+            if (this.EstaDespierto == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        
+        public Boolean PuedeDormir()
+        {
+            if (this.EstaDespierto == true)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Boolean PuedeDespertar()
+        {
+            if (this.EstaDespierto == true)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Boolean PuedeJugar()
+        {
+            if (this.EstaLleno == false)
+            {
+                return false;
+            }
+
+            if(this.EstaDespierto == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ MascotaTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace MascotaVirtual.Tests
+{
+    public class MascotaTests
+    {
+        
+        [Fact]
+        public void PuedeComer_EstaLLeno_DevuelveFalso()
+        {
+            //Arrange
+            Mascota mascota = new Mascota();
+            mascota.EstaLleno = true;
+
+            //Act
+            var resultado = mascota.PuedeComer();
+
+            //Assert
+            Assert.False(resultado);
+
+        }
+
+
+    }
+}
